7a5c8c57e614c71d7d1b7d2be8ce9611db7b7ee3
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-05-08 20:20:34 -0300

    parte 01

--- curso-angular/src/app/components/first-component/first-component.component.ts
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent implements OnInit {
+  name:string = "Jairo";
+  age: number = 41;
+  hobbies = ["Correr", "Jogar", "Estudar"];
+  car = {
+    name: "Polo",
+    year: 2019
+  }
 
   constructor() { }
 
